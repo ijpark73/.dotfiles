48406b15ac58f14204c4c113f875b4bce48118e0
ijparking <ijparking@korea.com>
2018-11-19 17:12:45 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,13 +8,19 @@ sudo apt-get install -y zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sudo chsh -s $(which zsh) $(whoami)
+#sudo chsh -s $(which zsh) $(whoami)
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "======================================================================"
+echo " Installing vim, Vundle plugin....                                        "
+echo "======================================================================"
+
+sudo apt-get install -y vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "======================================================================"
 echo " Copying dotfiles....                                                 "
